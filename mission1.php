a3aa8d95600c71afec1d31f28ae46e6b8740ed8e
kakashi <nk29time@gmail.com>
2021-06-02 13:38:50 +0900

    first

---
+++ mission1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mission1</title>
+</head>
+
+<body>
+  <form method="POST" action="mission1.php">
+    日本の首都は？<br>
+    <input type="text" name="check_data" value="" />
+    <input type="submit" name="send" value="OK" />
+  </form>
+  <?php
+if(!empty($_POST['send']) && $_POST['check_data'] === '東京'){
+  echo '正解';
+}else if(!empty($_POST['send']) ){
+  echo '不正解';
+}
+?>
+</body>
+
+</html>
